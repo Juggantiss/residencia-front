110985571791b8f3d12fb54f41c2f5cf6b1fa0ce
Juan Raul Martinez Lopez <juggantiss@gmail.com>
2022-07-19 23:00:26 -0500

    Agregado progreso al dashboard

--- src/pages/aspirant/DashboardAspirant.jsx
+++ src/pages/aspirant/DashboardAspirant.jsx
@@ -1,27 +1,61 @@
-import { Card } from "antd";
+import { Card, Col, Row, Progress } from "antd";
+import { RightCircleOutlined } from "@ant-design/icons";
 import "../../styles/DashboardAspirant.modules.css";
 import DashboardImage from "../../assets/img/dashboard-img.svg";
-
-const { Grid } = Card;
-// const { Text } = Typography;
+import { Link } from "react-router-dom";
 
 function DashboardAspirant({ data }) {
   return (
-    <Card className="banner_container">
-      <Grid className="banner_card" hoverable={false}>
-        <p>
-          Hola <span>{data?.usersPermissionsUser?.data?.attributes?.name}</span>
-          , bienvenido a la familia del CBTis 205
-        </p>
-        <p>
-          Estamos felices de que estés aquí, queremos{" "}
-          <span>crecer contigo</span>, acompañarte hacia tus metas y objetivos.
-        </p>
-      </Grid>
-      <Grid className="banner_card" hoverable={false}>
-        <img className="image" src={DashboardImage} alt="Dashboard" />
-      </Grid>
-    </Card>
+    <div>
+      <Row className="banner_container">
+        <Col className="banner_card" flex="auto">
+          <p>
+            Hola{" "}
+            <span>{data?.usersPermissionsUser?.data?.attributes?.name}</span>,
+            bienvenido a la familia del CBTis 205
+          </p>
+          <p>
+            Estamos felices de que estés aquí, queremos{" "}
+            <span>crecer contigo</span>, acompañarte hacia tus metas y
+            objetivos.
+          </p>
+        </Col>
+        <Col className="banner_card" flex="auto">
+          <div className="container-image">
+            <img className="image" src={DashboardImage} alt="Dashboard" />
+          </div>
+        </Col>
+      </Row>
+      <Row className="cards_container">
+        <Card
+          className="card_progress"
+          // headStyle={{ color: "#ffffff" }}
+          title="Progreso de formularios"
+          extra={
+            <Link to="/dashboard">
+              <RightCircleOutlined style={{ fontSize: "24px" }} />
+            </Link>
+          }
+        >
+          <Progress
+            strokeWidth={12}
+            width={240}
+            type="circle"
+            percent={100}
+            format={() => "Enviado"}
+          />
+        </Card>
+      </Row>
+    </div>
+
+    // <Card className="banner_container">
+    //   <Grid className="banner_card" hoverable={false}>
+
+    //   </Grid>
+    //   <Grid className="banner_card" hoverable={false}>
+    //     <img className="image" src={DashboardImage} alt="Dashboard" />
+    //   </Grid>
+    // </Card>
   );
 }
 
