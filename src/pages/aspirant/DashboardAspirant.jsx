13a03efa30a5dfc304eef6d835e49d0b7e58e98b
Juan Raul Martinez Lopez <juggantiss@gmail.com>
2022-07-28 14:37:31 -0500

    Agregado funcion para atrapar el porcentaje de avance

--- src/pages/aspirant/DashboardAspirant.jsx
+++ src/pages/aspirant/DashboardAspirant.jsx
@@ -3,20 +3,22 @@ import { RightCircleOutlined } from "@ant-design/icons";
 import "../../styles/DashboardAspirant.modules.css";
 import DashboardImage from "../../assets/img/dashboard-img.svg";
 import { Link } from "react-router-dom";
+import getProgressFormAspirant from "../../utils/getProgressFormAspirant";
 
 function DashboardAspirant({ data }) {
+  let percent = getProgressFormAspirant(data);
+  let name = data?.usersPermissionsUser?.data?.attributes?.name;
+
   return (
     <div>
       <Row className="banner_container">
         <Col className="banner_card" flex="auto">
           <p>
-            Hola{" "}
-            <span>{data?.usersPermissionsUser?.data?.attributes?.name}</span>,
-            bienvenido a la familia del CBTis 205
+            Hola <span>{name}</span>, bienvenido a la familia del CBTis 205
           </p>
           <p>
-            Estamos felices de que estés aquí, queremos{" "}
-            <span>crecer contigo</span>, acompañarte hacia tus metas y
+            Estamos felices de que estés aquí, queremos
+            <span> crecer contigo</span>, acompañarte hacia tus metas y
             objetivos.
           </p>
         </Col>
@@ -41,8 +43,8 @@ function DashboardAspirant({ data }) {
             strokeWidth={12}
             width={240}
             type="circle"
-            percent={100}
-            format={() => "Enviado"}
+            percent={percent[1]}
+            format={percent[1] === 100 && (() => "Enviado")}
           />
         </Card>
       </Row>
