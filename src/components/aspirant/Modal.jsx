3e23c4fcacf8025b13f83d5be44a9e67bf23c0f3
Juan Raul Martinez Lopez <juggantiss@gmail.com>
2022-08-18 22:56:53 -0500

    Cambio de boton cerrar en modal

--- src/components/aspirant/Modal.jsx
+++ src/components/aspirant/Modal.jsx
@@ -4,13 +4,12 @@ const Modal = ({ close, children }) => {
   return (
     <div className="modal-open modal-bottom sm:modal-middle flex justify-center fixed top-0 w-full h-full">
       <div className="modal-box">
-        <label
-          htmlFor="my-modal-3"
+        <button
           className="btn btn-sm btn-circle absolute right-2 top-2"
           onClick={close}
         >
           ✕
-        </label>
+        </button>
         {children}
         <div className="divider"></div>
         <div className="modal-action">
